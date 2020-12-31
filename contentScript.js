36ee8b9e4cfd90155fc295879508a47077064023
Milo Silva <milo.silva234@gmail.com>
2020-12-31 20:54:35 +0100

    test iframe implementation

--- contentScript.js
+++ contentScript.js
@@ -1,42 +1,35 @@
-// inject the inline style header
+// inject the inline style element
 const chatStyle = document.createElement("style");
 
 chatStyle.innerHTML = `.video-content__video-content-container--376eu {
   right: 150px;
 }`;
 
-document.head.appendChild();
+// script to reposition video player:
 
-// inject The chat UI into the page.
-const chatUI = document.createElement("div");
-chatUI.style.cssText = `background: white; borderRadius: 5px; width: 20vw; height: 75vh; position: absolute; top: 0; right: 0;`;
-const vidContainer = document.querySelector(".main__main-page-layout--yEFl9>div");
-vidContainer.appendChild(chatUI);
 const vidDiv = (document.querySelector(
   ".main__main-page-layout--yEFl9>div"
 ).children[0].style.cssText = `position: relative; right: 160px`);
-chatUI.innerHTML = `
-  <div id='message-area' style='height: 90%;'>
-    <ul id="messageList" style='list-style-type: none;margin: 0;padding: 0; color: black; font-size: 14px; height:-webkit-fill-available' ></ul>
-  </div>
-  <form id="m"
-    style="padding: 3px;position: absolute;bottom: 0;width: 100%; margin-bottom: 2.5%;"
-  >
-    <input id='msg-input'
-      style="border: 0;padding: 10px;width: 84%;margin-right: 0.5%;" 
-      placeholder='what was Wenger thinking?' autocomplete'off' type='text'/>
-      <button 
-      style="
-      width: 14%;
-      background:rgb(130, 224, 255);
-      border: none;
-      border-radius: 5px;
-      padding: 10px;" >
-      send</button>
-  </form>
-`;
 
-const msgList = document.querySelector("#messageList");
+const script = document.createElement("script");
+const code = `
+const vidDiv = document.querySelector(".main__main-page-layout--yEFl9>div").children[0];
+vidDiv.style.cssText = 'position: relative; right: 160px';
+`;
+script.text = code;
+document.head.appendChild(script);
+
+// inject The chat-UI as an iframe into the page.
+
+const chatUI = document.createElement("iframe");
+chatUI.setAttribute("frameborder", 0);
+chatUI.style.cssText = `borderRadius: 5px; width: 20vw; height: 75vh; position: absolute; top: 0; right: 0;`;
+const vidContainer = document.querySelector(".main__main-page-layout--yEFl9>div");
+vidContainer.appendChild(chatUI);
+
+chatUI.setAttribute("src", "localhost:3000");
+
+/* const msgList = document.querySelector("#messageList");
 
 function addMsg(msg, parent) {
   const newMsg = document.createElement("li");
@@ -50,3 +43,4 @@ document.querySelector("#m").onsubmit = (event) => {
   addMsg(message, msgList);
   event.target[0].value = "";
 };
+ */
