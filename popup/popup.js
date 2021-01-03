5c7b1cf58ed7d315ec833433ce65236cf74ebc73
Milo Silva <milo.silva234@gmail.com>
2021-01-03 22:37:04 +0100

    Refactor method to inject content-script

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,17 @@
 globalThis.popup = true;
 try {
-  chrome.tabs.executeScript(
-    globalThis.activeTabId,
-    {
-      file: "contentScript.js",
-    },
-    () => {
-      if (chrome.runtime.lastError) return console.log("error:", chrome.runtime.lastError);
-      console.log("content-script injected");
-    }
-  );
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {
+        file: "contentScript.js",
+      },
+      () => {
+        if (chrome.runtime.lastError) return console.log("error:", chrome.runtime.lastError);
+        console.log("content-script injected");
+      }
+    );
+  });
 } catch (e) {
   console.log(e);
 }
